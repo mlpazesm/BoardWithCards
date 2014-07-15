093b843b6e36cbf1b651b04aeff9ea7abdd17745
fonter <fonter@fonter-PC.(none)>
2014-07-15 20:57:38 +1000

    change default card

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ FPS = 15
 WINDOW_SIZE = (640, 480)
 
 #folders ('cards_mailru' or 'cards_classic')
-CARDS_FOLDER = 'cards_classic'
+CARDS_FOLDER = 'cards_mailru'
 
 #colors
 TABLE_COLOR = (0x23, 0x1F, 0x20)
